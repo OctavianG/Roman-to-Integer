f6c6763b424a62eddb4b849db76cffec83e4c273
OctavianG <octavian7.g@gmail.com>
2023-07-18 18:53:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+//    Input: s = "MCMXCIV"
+//    Output: 1994
+//    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+    public static int romanToInt(String s) {
+        int answer = 0, number = 0, prev = 0;
+
+        for (int j = s.length() - 1; j >= 0; j--) {
+            switch (s.charAt(j)) {
+                case 'M' -> number = 1000;
+                case 'D' -> number = 500;
+                case 'C' -> number = 100;
+                case 'L' -> number = 50;
+                case 'X' -> number = 10;
+                case 'V' -> number = 5;
+                case 'I' -> number = 1;
+            }
+            if (number < prev) {
+                answer -= number;
+            } else {
+                answer += number;
+            }
+            prev = number;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        System.out.println(romanToInt(s));
+    }
+}
